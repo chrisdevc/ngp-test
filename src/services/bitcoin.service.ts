52839a424d21f17e18955c326b8c17726d1bdedd
Chris Ciantar <chris.ciantar@riverigaming.com>
2020-03-10 13:08:33 +0100

    docs(*):  add todo

--- src/services/bitcoin.service.ts
+++ src/services/bitcoin.service.ts
@@ -4,6 +4,7 @@ import { Axios } from "axios-observable";
 const API_URL =
   "https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2019-12-31";
 
+  // todo: this would ideally be injected in component using DI
 export class BitcoinService {
   getData$() {
     return Axios.get(API_URL);
